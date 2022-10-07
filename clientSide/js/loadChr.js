f61838932bb0b13f605dda3c4e7c6fae427fc29f
Jack Wilson <jackawilson4221@gmail.com>
2022-10-07 16:23:18 -0500

    did a lot styling, made values changeable

--- clientSide/js/loadChr.js
+++ clientSide/js/loadChr.js
@@ -1,24 +1,27 @@
 const yogBtn = document.getElementById('characterBtn')
+const baseUrl = window.location.origin
+const host = window.location.host
+console.log(baseUrl)
+console.log(host)
 
 yogBtn.addEventListener('click', async () => {
-  const characterUrl = 'http://localhost:8001/js/campaigns/test/characters/PCs/Yog.json'
+  const characterUrl = baseUrl + '/js/campaigns/test/characters/PCs/Yog.json'
   const response = await fetch(characterUrl)
   const yogJson = await response.text()
   const yog = JSON.parse(yogJson)
-  console.log(yog)
   document.body.appendChild(createSheet(yog))
 })
 
 function createSheet (chr) {
   const characterSheet = document.createElement('div')
-  characterSheet.id = 'sheet'
+  characterSheet.className = 'sheet'
   characterSheet.appendChild(boxer(chr))
   return characterSheet
 }
 
 function createBox (boxName) {
   const box = document.createElement('div')
-  box.id = boxName
+  box.className = boxName
   box.innerHTML = `<h1>${boxName}:</h1>`
   return box
 }
@@ -28,17 +31,28 @@ function boxer (obj) {
   Object.keys(obj).forEach((thing, i) => {
     const thingsBox = createBox(thing)
     const thingsValue = Object.values(obj)[i]
-    console.log(thingsValue)
     if (typeof thingsValue !== 'object') {
-      const valueElement = document.createElement('p')
-      valueElement.innerHTML = `${thingsValue}`
-      thingsBox.appendChild(valueElement)
-      console.log('showing value')
+      thingsBox.appendChild(inputMaker(thingsValue))
     } else {
-      console.log('gonna need a new box')
       thingsBox.appendChild(boxer(thingsValue))
     }
     box.appendChild(thingsBox)
   })
   return box
+}
+function inputMaker (value) {
+  const valueElement = document.createElement('input')
+  if (typeof value !== 'boolean') {
+    valueElement.setAttribute('type', 'text')
+    valueElement.setAttribute('value', `${value}`)
+    const inputSize = `${value}`.length
+    valueElement.setAttribute('size', `${inputSize + 2}`)
+    return valueElement
+  } else {
+    valueElement.setAttribute('type', 'checkbox')
+    if (value == true){
+      valueElement.setAttribute('checked', '')
+    }
+    return valueElement
+  }
 }
--- server.js
+++ server.js
@@ -9,16 +9,32 @@ app.use(express.static(path2site))
 
 // 3126952310
 
+const { networkInterfaces } = require('os');
+const nets = networkInterfaces();
+const results = {}
+for (const name of Object.keys(nets)) {
+  for (const net of nets[name]) {
+      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+      // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+      const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+      if (net.family === familyV4Value && !net.internal) {
+          if (!results[name]) {
+              results[name] = [];
+          }
+          results[name].push(net.address);
+      }
+  }
+}
 app.listen(
   port,
   () => {
-    console.log(`Dance floor is at http://localhost:${port}`)
+    console.log(`Dance floor is at http://${results.en0[0]}:${port}`)
   }
 )
 
 app.get('/', (req, res) => {
-  res.status(200).send('howdy')
-  console.log('someone came to boogy at' + new Date())
+  res.sendFile(path.join(__dirname, '/clientSide/index.html'))
+  console.log('someone came to boogy at ' + new Date())
 })
 
 app.get('/knock', (req, res) => {
@@ -28,7 +44,7 @@ app.get('/knock', (req, res) => {
   console.log('there is a knock at the door')
 })
 
-app.get('/js/campaigns/test/characters/PCs/:name', (req, res, next) => {
+app.get('/clientSide/js/campaigns/test/characters/PCs/:name', (req, res, next) => {
   var characterName = req.params.name
   res.status(200).sendFile(characterName, function (err) {
     if (err) {
